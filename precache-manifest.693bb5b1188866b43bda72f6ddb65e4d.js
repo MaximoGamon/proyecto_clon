74115a9fa069e91e7e1837fed3229ca442c797e6
Maximo Gamon <maximo_3141240179@utd.edu.mx>
2026-06-09 21:12:21 -0600

    Cambios 9 de junio

---
+++ precache-manifest.693bb5b1188866b43bda72f6ddb65e4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0be56d6603c7382c19f",
+    "url": "/proyecto_clon/static/css/main.1cab1ad7.chunk.css"
+  },
+  {
+    "revision": "e0be56d6603c7382c19f",
+    "url": "/proyecto_clon/static/js/main.b4382e4a.chunk.js"
+  },
+  {
+    "revision": "d2035e8152ac00c6ab7d",
+    "url": "/proyecto_clon/static/js/runtime~main.446d7e4c.js"
+  },
+  {
+    "revision": "ed329e59f8d0d73dd1ab",
+    "url": "/proyecto_clon/static/js/2.6245a9fe.chunk.js"
+  },
+  {
+    "revision": "8f0a216d299d80c4b097c8dd1eda7474",
+    "url": "/proyecto_clon/index.html"
+  }
+];
